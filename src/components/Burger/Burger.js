aedf57fdf65b6e688c389c018175fd2dee2901b3
Alex Ferrer <aferrerdev@gmail.com>
2019-10-27 12:51:43 +0100

    Starting the Burger Component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = (props) => (
-    <Aux>
-        
-    </Aux>
-);
+const burger = (props) => {
+    return (
+        <div className="Burger">
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    )
+};
 
 export default burger;
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import classes from './burgerIngredient.css'
+import './BurgerIngredient.css'
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component {
@@ -8,35 +8,35 @@ class BurgerIngredient extends Component {
         switch (this.props.type) {
             case ('bread-bottom'):
                 ingredient = (
-                    <div className={classes.BreadBottom}></div>
+                    <div className="BreadBottom"></div>
                 );
                 break;
             case ('bread-top'):
                 ingredient = (
-                    <div className={classes.BreadTop}>
-                        <div className={classes.Seeds1}></div>
-                        <div className={classes.Seeds2}></div>
+                    <div className="BreadTop">
+                        <div className="Seeds1"></div>
+                        <div className="Seeds2"></div>
                     </div>
                 );
                 break;
             case ('meat'):
                 ingredient = (
-                    <div className={classes.Meat}></div>
+                    <div className="Meat"></div>
                 );
                 break;
             case ('cheese'):
                 ingredient = (
-                    <div className={classes.Cheese}></div>
+                    <div className="Cheese"></div>
                 );
                 break;
             case ('bacon'):
                 ingredient = (
-                    <div className={classes.Bacon}></div>
+                    <div className="Bacon"></div>
                 );
                 break;
             case ('salad'):
                 ingredient = (
-                    <div className={classes.Salad}></div>
+                    <div className="Salad"></div>
                 );
                 break;
             default:
